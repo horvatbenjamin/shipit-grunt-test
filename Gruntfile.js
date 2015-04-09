fc74fc62c785c71ce1abe8e99eb2e8af1de3ad65
Benjamin Horvat <benjamin@benjamin-laptop>
2015-04-09 14:21:58 +0200

    qwerty

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,9 +55,7 @@ module.exports = function (grunt) {
   grunt.registerTask('start', function () {
 //    var done = this.async();
     var current = grunt.config('shipit.options.deployTo') + '/current';
-    grunt.shipit.remote('cd ' + current + ' && npm start').then(function (res) {
-      console.log(res[0].stdout);
-    });
+    return grunt.shipit.remote('cd ' + current + ' && npm start')
   });
 
    /**
@@ -75,13 +73,9 @@ module.exports = function (grunt) {
   grunt.registerTask('stop', function () {
 //    var done = this.async();
     var current = grunt.config('shipit.options.deployTo') + '/current';
-    grunt.shipit.remote('cd ' + current + ' && node_modules/pm2/bin/pm2 stop \\$(cat ./REVISION)').then(function (res) {
-      console.log(res[0].stdout);
-    });
+    grunt.shipit.remote('cd ' + current + ' && node_modules/pm2/bin/pm2 stop \\$(cat ./REVISION)');
 
-    grunt.shipit.remote('cd ' + current + ' && node_modules/pm2/bin/pm2 delete \\$(cat ./REVISION)').then(function (res) {
-      console.log(res[0].stdout);
-    });
+    grunt.shipit.remote('cd ' + current + ' && node_modules/pm2/bin/pm2 delete \\$(cat ./REVISION)');
   });
   
   grunt.shipit.on('updated', function() {
