3d219a728e8dc4fef80dae29db6e97cc02f745b7
Benjamin Horvat <benjamin@benjamin-laptop>
2015-04-12 13:13:46 +0200

    aaa

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express()
 console.log('Starting....');
  
 app.get('/', function (req, res) {
-  res.send('Hello World 12345');
+  res.send('Sziasztok');
   console.log('request...');
 })
  
